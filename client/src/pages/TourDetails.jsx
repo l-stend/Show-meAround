c661d798ee1d7860ef12eef24606901372120345
l-stend <stendardo.lu@gmail.com>
2022-06-11 11:38:25 +0200

    add paricipation, non deleting yet

--- client/src/pages/TourDetails.jsx
+++ client/src/pages/TourDetails.jsx
@@ -3,18 +3,28 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getAllTours, updateTour } from '../features/tours/toursSlice';
+import uniqid from 'uniqid';
+import { toast } from 'react-toastify';
 
 const TourDetails = () => {
   const { toursArr, isLoading } = useSelector((store) => store.tours);
   const { user } = useSelector((store) => store.user);
   const [tour, setTour] = useState(null);
   const [content, setContent] = useState('');
+  const [participating, setParticipating] = useState(false);
   const params = useParams();
   const dispatch = useDispatch();
   let selected = params.tourId;
 
+  useEffect(() => {
+    let selectedTour = toursArr.filter((item) => item._id === selected)[0];
+    setTour(selectedTour);
+  }, []);
+
   const submitReview = (id) => {
     const newReview = {
+      id: uniqid(),
+      name: user.name,
       content: content,
       vote: 1,
     };
@@ -28,10 +38,26 @@ const TourDetails = () => {
     dispatch(updateTour(updates));
   };
 
-  useEffect(() => {
-    let selectedTour = toursArr.filter((item) => item._id === selected)[0];
-    setTour(selectedTour);
-  }, []);
+  const signForTour = (id) => {
+    const participant = {
+      name: user.name,
+      email: user.email,
+    };
+
+    const existingParticipants = tour.participants;
+    const updates = {
+      _id: id,
+      participants: [...existingParticipants, participant],
+    };
+
+    if (!existingParticipants.some((item) => item.email === user.email)) {
+      dispatch(updateTour(updates));
+      setParticipating(!participating);
+      return;
+    } else {
+      alert('already participating');
+    }
+  };
 
   return (
     <section>
@@ -40,9 +66,12 @@ const TourDetails = () => {
       <h2>{tour?._id}</h2>
       <div>
         <div>
-          {user.type === 'traveler' && (
-            <button onClick={() => console.log('participate')}>
-              Participate
+          {user.type === 'traveler' && !participating && (
+            <button onClick={() => signForTour(selected)}>Participate</button>
+          )}
+          {user.type === 'traveler' && participating && (
+            <button onClick={() => setParticipating(!participating)}>
+              cancel
             </button>
           )}
         </div>
