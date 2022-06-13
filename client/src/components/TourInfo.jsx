bd0b250de27373ec964c871d33072b2fea3205f3
l-stend <stendardo.lu@gmail.com>
2022-06-13 11:13:06 +0200

    tour info in tour page

--- client/src/components/TourInfo.jsx
+++ client/src/components/TourInfo.jsx
@@ -7,6 +7,7 @@ const TourInfo = ({ tour }) => {
   return (
     <section>
       <h2>{tour?.title}</h2>
+      {/* TIME INFO */}
       <div className='time info'>
         <p>
           {tour?.days.map((day) => (
@@ -16,24 +17,21 @@ const TourInfo = ({ tour }) => {
         <p>Starts at: {tour?.startAt}</p>
         <p>Duration: {tour?.duration} hours</p>
       </div>
+      {/* DESCRIPTION */}
       <div className='description'>
         <p>{tour?.description}</p>
+      </div>
+      {/* AUTHOR INFO */}
+      <div className='author-info'>
+        <p>
+          Hosted by: {tour?.author.name} {tour?.author.lastName}
+        </p>
+        <p>
+          About {tour?.author.name}: {tour?.author.aboutMe}
+        </p>
       </div>
     </section>
   );
 };
 
 export default TourInfo;
-
-// const { toursArr, isLoading } = useSelector((store) => store.tours);
-// const { user } = useSelector((store) => store.user);
-// const [tour, setTour] = useState(null);
-// const dispatch = useDispatch();
-// const params = useParams();
-
-// useEffect(() => {
-//   dispatch(getAllTours());
-//   let selectedTour = toursArr.filter((item) => item._id === selected)[0];
-//   setTour(selectedTour);
-//   console.log('from info component   ', tour);
-// }, []);
